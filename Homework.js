b9782ad33d83ab2fd3f3d7e05a856c102f08ad77
DatDong <phatdat16102002@gmail.com>
2025-03-25 22:15:23 +0700

    Fixed cameCase, translate English

--- Homework.js
+++ Homework.js
@@ -1,16 +1,12 @@
 //1.. Difference(s) between == and ===
-console.log("Sự khác nhau giữa == và === là so sánh kiểu dữ liệu");
-console.log(5 == "5"); // truong hop == => true, tại nó chuyển kiểu dữ liệu trước khi so sánh
-console.log(5 === "5"); //truong hop === => false, tại nó KHÔNG chuyển kiểu dữ liệu trước khi so sánhsánh
-/*2.. Use variables and operators to:
-Calculate BMI (Body Mass Index)
-Calculate Simple Interest (principal * rate * time)
-Convert Currency (USD to local currency)
-Calculate Time (hours:minutes:seconds) */
+console.log("The difference between == and === is type comparison");
+console.log(5 == "5"); // In the case of == => true, because it converts the data type before comparing
+console.log(5 === "5"); // In the case of === => false, because it does NOT convert the data type before comparing
+
 const height = 1.78;
 const weight = 80;
 const bmi = weight / (height * height);
-console.log("BMI: " + bmi)
+console.log("BMI: " + bmi);
 
 const principal = 10000;
 const rate = 0.05;
@@ -18,22 +14,13 @@ const time = 2;
 const interest = principal * rate * time;
 console.log("Interest = " + interest);
 
-const exchangeRate = 255000; 
-const amountInUSD = 100; 
+const exchangeRate = 255000;
+const amountInUSD = 100;
 const amountInVND = amountInUSD * exchangeRate;
 console.log(`${amountInUSD} USD = ${amountInVND} VND`);
 
-const now = new Date();
-const hours = now.getHours().toString().padStart(2, '0');
-const minutes = now.getMinutes().toString().padStart(2, '0');
-const seconds =  now.getSeconds().toString().padStart(2, '0');
-const time1 = `${hours}:${minutes}:${seconds}`;
-console.log("Time1: ", now);
-/*
-#Additional challenges:
-Add more variables to make calculations more complex
-Try using different mathematical operators
-Print results in different formats
-Think about edge cases (negative numbers, zero, etc.)
-Compare let, const, var
- */
+let totalSeconds = 500;
+let hours = Math.floor(totalSeconds / 3600);
+let minutes = Math.floor((totalSeconds % 3600) / 60);
+let seconds = totalSeconds % 60;
+console.log(`Time: ${hours}h ${minutes}m ${seconds}s`);
